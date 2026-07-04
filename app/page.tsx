546fe895cdf65362603c7af3588cebd10a4ce83d
IldarIskhakovM <iskhakovim@gmail.com>
2026-07-04 11:53:50 +0300

    Add Header component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 </> TypeScript
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Синергия</h1>
-      <p>АСУ ТП и промышленная автоматизация</p>
-    </main>
+    <>
+      <Header />
+      <main style={{ padding: 40, fontFamily: "Arial" }}>
+        <h1>Синергия</h1>
+        <p>АСУ ТП и промышленная автоматизация</p>
+      </main>
+    </>
   );
 }
