f7cb2fb424c6e9e1c0b2467fb2e86842d9697fe8
IldarIskhakovM <iskhakovim@gmail.com>
2026-07-04 12:08:35 +0300

    Add Projects component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Services />
+      <Projects />
     </>
   );
 }
